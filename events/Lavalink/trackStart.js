6d179cb1db05e3f92d0b25e5fc4d83afc7fa1396
Priyanshu360-cpu <79325092+Priyanshu360-cpu@users.noreply.github.com>
2021-09-24 21:46:52 +0530

    selectmenu => trackStart event applied => option filters

--- events/Lavalink/trackStart.js
+++ events/Lavalink/trackStart.js
@@ -1,4 +1,4 @@
-const { MessageEmbed } = require("discord.js");
+const { MessageEmbed, MessageSelectMenu } = require("discord.js");
 const { convertTime } = require('../../utils/convert.js');
 const { MessageActionRow, MessageButton} = require('discord.js');
 module.exports = async (client, player, track, payload) => {
@@ -31,6 +31,50 @@ module.exports = async (client, player, track, payload) => {
             .setLabel('Loop')
             .setStyle('SECONDARY')
             .setEmoji('881060715941740555'),
+    )
+    const column = new MessageActionRow()
+    .addComponents(
+            new MessageSelectMenu()
+            .setCustomId('filter')
+            .setPlaceholder('Apply Filters')
+            .addOptions([
+                {
+                    label: 'Party mode',
+							description: 'toggle Party mode for this song',
+							value: 'first_option',
+                },
+                {
+                    label: 'Bass mode',
+							description: 'toggle Bass mode for this song',
+							value: 'second_option',
+                },
+                {
+                    label: 'Radio mode',
+							description: 'toggle Radio mode for this song',
+							value: 'third_option',
+                },
+                {
+                    label: 'Pop mode',
+							description: 'toggle Pop mode for this song',
+							value: 'fourth_option',
+                },
+                {
+                    label: 'Trablebass mode',
+							description: 'toggle Trablebass mode for this song',
+							value: 'fifth_option',
+                },
+                {
+                    label: 'Soft mode',
+							description: 'toggle Soft mode for this song',
+							value: 'sixth_option',
+                },
+                {
+                    label: 'Clear Filters',
+							description: 'Clears filter for the song',
+							value: 'seventh_option',
+                },
+
+            ])
     );
     
     const thing = new MessageEmbed()
@@ -39,10 +83,10 @@ module.exports = async (client, player, track, payload) => {
         .setDescription(`<a:diska:876363187119857687> **Started Playing**\n \`\`\`\n${track.title}\`\`\`\n**Duration** - \`[${convertTime(track.duration)}]\`\n**Requested By** - [<@${track.requester.id}>]`)
         .setImage(track.displayThumbnail("hqdefault"))
         .setColor(client.embedColor)
-    return channel.send({embeds: [thing], components: [row]}).then(sent => { 
+    return channel.send({embeds: [thing], components: [row, column]}).then(sent => { 
         let id = sent.id;
         setTimeout(() => channel.messages.delete(id), `${track.duration}`);
-      });
+      }); 
 
       
 }
