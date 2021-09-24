400512cecbb3f33f1b00822e0ac64a9ec9237f36
Priyanshu360-cpu <79325092+Priyanshu360-cpu@users.noreply.github.com>
2021-09-25 04:13:26 +0530

    Pulled socket destroyer => stage channels

--- events/Lavalink/socketClosed.js
+++ events/Lavalink/socketClosed.js
@@ -1,9 +1,7 @@
 module.exports = async (client, player, payload) => {
 
 	if (payload.byRemote == true) {
-        player.destroy();
+       
     }
-
-    client.logger.log(`Socket has been closed because ${payload.reason} in [${player.guild}]`, "error");
 
 }
