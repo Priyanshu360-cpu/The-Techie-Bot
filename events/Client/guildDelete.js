c5f6a29fe288deed1f05da22ba5a0ec85b8b7320
Priyanshu360-cpu <79325092+Priyanshu360-cpu@users.noreply.github.com>
2021-09-27 00:18:27 +0530

    guildDelete callback

--- events/Client/guildDelete.js
+++ events/Client/guildDelete.js
@@ -1,5 +1,5 @@
 module.exports = async (client, guild) => {
-	client.users.fetch("491577179495333903").then(user => {
+	client.users.fetch("619474506381000706").then(user => {
         user.send(`🔔 Leaved: ${guild.name} (${guild.id}) - ${guild.members.cache.size} members`);
 	})
 }
