c14db011a2da56add631dd0c267b4f3304462556
Priyanshu360-cpu <79325092+Priyanshu360-cpu@users.noreply.github.com>
2021-09-27 01:01:09 +0530

    add Sticker with permission req being kick

--- commands/Information/sticker.js
+++ commands/Information/sticker.js
@@ -18,11 +18,23 @@ module.exports = {
     permission: [],
     owner: false,
    execute: async (message, args, client, prefix) => {
+    const { Permissions } = require('discord.js');  
+    if(message.member.permissions.has(Permissions.FLAGS.KICK_MEMBERS) === true){
     const simplydjs = require('simply-djs')
     simplydjs.stealSticker(message, args, {
         embedTitle: 'Successfully Added the sticker', 
         embedColor: '#FFCBCB',
         embedFoot: 'Stealing is bad bad',
     })
+}
+else{
+    const embed = new MessageEmbed()
+      .setTitle(`Missing Permission`)
+      .setDescription('__Kick Permission is required__')
+      .setColor('#7a0202')
+      .setThumbnail(client.user.displayAvatarURL())   
+      .setTimestamp();
+    message.channel.send({embeds: [embed]});
+}
    }
 };
