c4db782dfff09d5ee735f43c4177de6d13672ae4
Priyanshu360-cpu <tpriyanshu775@gmail.com>
2021-12-02 02:03:52 +0530

    minor fix to music status as combined

--- events/Lavalink/trackStart.js
+++ events/Lavalink/trackStart.js
@@ -62,289 +62,31 @@ module.exports = async (client, player, track, payload, message, songt) => {
         const emojiplay = client.emoji.play;
     player.setVolume('100'); 
     
-    const collector = channel.createMessageComponentCollector({time: 300000});
-collector.on('collect', async i => 
-{
-        if (!i.isButton()) return;
-            if (i.customId === 'Pausea') {
-                if (player && i.member.voice.channel !== i.guild.me.voice.channel) {
-                    let thing = new MessageEmbed()
-                        .setColor("RED")
-                        .setDescription(`You must be in the same channel as ${i.client.user}`);
-                        await i.reply({embeds: [thing], ephemeral: true });}
-                     else {
-                player.pause(true);
-                let thing = new MessageEmbed()
-                .setColor('#00ffd2')
-                .setTimestamp()
-                .setDescription(`${i.member} **Paused** the song successfully`)
-                await i.reply({embeds: [thing]});
-                     }
-            }
-            if (i.customId === 'Resumea') {
-                if (player && i.member.voice.channel !== i.guild.me.voice.channel) {
-                    let thing = new MessageEmbed()
-                        .setColor("RED")
-                        .setDescription(`You must be in the same channel as ${i.client.user}`);
-                        await i.reply({embeds: [thing], ephemeral: true });}
-                        else{
-                player.pause(false);
-                let thing = new MessageEmbed()
-                .setColor('#00ffd2')
-                .setTimestamp()
-                .setDescription(`${i.member} **Resumed** the song successfully`)
-                await i.reply({embeds: [thing]});
-            }
-        }
-            if (i.customId === 'Skipa') {
-                if (player && i.member.voice.channel !== i.guild.me.voice.channel) {
-                    let thing = new MessageEmbed()
-                        .setColor("RED")
-                        .setDescription(`You must be in the same channel as ${i.client.user}`);
-                        await i.reply({embeds: [thing], ephemeral: true });}
-                        else{
-                const autoplay = player.get("autoplay");
-                const song = player.queue.current;
-        
-                if (autoplay === false) {
-                    player.stop();
-                } else {
-                    player.stop();
-                    player.queue.clear();
-                    player.set("autoplay", false);
-                }
-                let thing = new MessageEmbed()
-                .setDescription(`${i.member} **Skipped**\n[${song.title}](${song.uri})`)
-                .setColor('#00ffd2')
-                .setTimestamp()
-             await i.reply({embeds: [thing]});
-            }
-        }
-            if (i.customId === 'Lyricsa') {
-                const song = player.queue.current;
-                let c = song.title;
-                let d = 'https://api.leref.ga/lyrics?song='+c;
-                const axios = require('axios');
-                let response, data;
-                try {
-                    response = await axios.get(d);
-                    data = response.data;
-                }  catch (e) {
-                    let bhing = new MessageEmbed()
-                    .setDescription('Unable to find lyrics')
-                    .setColor('#00ffd2')
-                    .setTimestamp()
-                    await i.reply({embeds: [bhing], ephemeral: true });
-                }
-        let du = data.lyrics;
-        let thing = new MessageEmbed()
-                .setDescription(du)
-                .setColor('#00ffd2')
-                .setTimestamp()
-                await i.reply({embeds: [thing]});
-            }
-            if (i.customId === 'Loopa') {
-                if (player && i.member.voice.channel !== i.guild.me.voice.channel) {
-                    let thing = new MessageEmbed()
-                        .setColor("RED")
-                        .setDescription(`You must be in the same channel as ${i.client.user}`);
-                        await i.reply({embeds: [thing], ephemeral: true });}
-                        else{
-                player.setTrackRepeat(!player.trackRepeat);
-                const trackRepeat = player.trackRepeat ? "enabled" : "disabled";
-                let thing = new MessageEmbed()
-                    .setColor('#00ffd2')
-                    .setTimestamp()
-                    .setDescription(` ${i.member} Loop track is now **${trackRepeat}**`)
-                    await i.reply({embeds: [thing]});
-                        }
-            }
-        }); 
-        const tollector = channel.createMessageComponentCollector({time: 300000});
-tollector.on('collect', async interaction => 
-{
-            if (!interaction.isSelectMenu()) return;
-        
-            if (interaction.customId === 'filter') {
-                const player = interaction.client.manager.get(interaction.guild.id);
-                if (player && interaction.member.voice.channel !== interaction.guild.me.voice.channel) {
-                    let thing = new MessageEmbed()
-                        .setColor("RED")
-                        .setDescription(`You must be in the same channel as ${interaction.client.user}`);
-                        await interaction.reply({embeds: [thing], ephemeral: true });}
-                        else{
-                if(interaction.values[0] === 'first_option'){
-                    var bands = [
-                        { band: 0, gain: -1.16 },
-                        { band: 1, gain: 0.28 },
-                        { band: 2, gain: 0.42 },
-                        { band: 3, gain: 0.5 },
-                        { band: 4, gain: 0.36 },
-                        { band: 5, gain: 0 },
-                        { band: 6, gain: -0.3 },
-                        { band: 7, gain: -0.21 },
-                        { band: 8, gain: -0.21 } 
-                    ];
-                    player.setEQ(...bands);
-                    let thing = new MessageEmbed()
-                    .setColor("#00FFD2")
-                    .setDescription(`${interaction.member} Applied the Filter Party Mode`);
-                await interaction.reply({embeds: [thing]});
-                }
-                if(interaction.values[0] === 'second_option'){
-                    var bands = [
-                        { band: 0, gain: 0.6 },
-                        { band: 1, gain: 0.7 },
-                        { band: 2, gain: 0.8 },
-                        { band: 3, gain: 0.55 },
-                        { band: 4, gain: 0.25 },
-                        { band: 5, gain: 0 },
-                        { band: 6, gain: -0.25 },
-                        { band: 7, gain: -0.45 },
-                        { band: 8, gain: -0.55 },
-                        { band: 9, gain: -0.7 },    
-                        { band: 10, gain: -0.3 },    
-                        { band: 11, gain: -0.25 },
-                        { band: 12, gain: 0 },   
-                        { band: 13, gain: 0 },
-                        { band: 14, gain: 0 }    
-                    ];
-                    player.setEQ(...bands);
-                    let thing = new MessageEmbed()
-                    .setColor("#00FFD2")
-                    .setDescription(`${interaction.member} Applied the Filter Bass Mode`);
-                await interaction.reply({embeds: [thing]});
-                }
-                if(interaction.values[0] === 'third_option'){
-                    var bands = [
-                        { band: 0, gain: 0.65 },
-                        { band: 1, gain: 0.45 },
-                        { band: 2, gain: -0.45 },
-                        { band: 3, gain: -0.65 },
-                        { band: 4, gain: -0.35 },
-                        { band: 5, gain: 0.45 },
-                        { band: 6, gain: 0.55 },
-                        { band: 7, gain: 0.6 },
-                        { band: 8, gain: 0.6 },
-                        { band: 9, gain: 0.6 },    
-                        { band: 10, gain: 0 },    
-                        { band: 11, gain: 0 },
-                        { band: 12, gain: 0 },   
-                        { band: 13, gain: 0 },
-                        { band: 14, gain: 0 }  
-                    ];
-                    player.setEQ(...bands);
-                    let thing = new MessageEmbed()
-                    .setColor("#00FFD2")
-                    .setDescription(`${interaction.member} Applied the Filter Radio Mode`);
-                await interaction.reply({embeds: [thing]});
-            }
-            if(interaction.values[0] === 'fourth_option'){
-                var bands = [
-                    { band: 0, gain: -0.25 },
-                    { band: 1, gain: 0.48 },
-                    { band: 2, gain: 0.59 },
-                    { band: 3, gain: 0.72 },
-                    { band: 4, gain: 0.56 },
-                    { band: 5, gain: 0.15 },
-                    { band: 6, gain: -0.24 },
-                    { band: 7, gain: -0.24 },
-                    { band: 8, gain: -0.16 },
-                    { band: 9, gain: -0.16 },    
-                    { band: 10, gain: 0 },    
-                    { band: 11, gain: 0 },
-                    { band: 12, gain: 0 },   
-                    { band: 13, gain: 0 },
-                    { band: 14, gain: 0 }
-                ];
-                player.setEQ(...bands);
-                let thing = new MessageEmbed()
-                    .setColor("#00FFD2")
-                    .setDescription(`${interaction.member} Applied the Filter Pop Mode`);
-                await interaction.reply({embeds: [thing]});
-            }
-            if(interaction.values[0] === 'fifth_option'){
-                var bands = [
-                    { band: 0, gain: 0.6 },
-                    { band: 1, gain: 0.67 },
-                    { band: 2, gain: 0.67 },
-                    { band: 3, gain: 0 },
-                    { band: 4, gain: -0.5 },
-                    { band: 5, gain: 0.15 },
-                    { band: 6, gain: -0.45 },
-                    { band: 7, gain: 0.23 },
-                    { band: 8, gain: 0.35 },
-                    { band: 9, gain: 0.45 },
-                    { band: 10, gain: 0.55 },
-                    { band: 11, gain: 0.6 },
-                    { band: 12, gain: 0.55 },
-                    { band: 13, gain: 0 },
-                    { band: 14, gain: 0 }
-                ];
-                player.setEQ(...bands);
-                let thing = new MessageEmbed()
-                    .setColor("#00FFD2")
-                    .setDescription(`${interaction.member} Applied the Filter Trablebass Mode`);
-                await interaction.reply({embeds: [thing]});
-            }
-            if(interaction.values[0] === 'sixth_option'){
-                var bands =  [
-                    { band: 0, gain: 0 },
-                    { band: 1, gain: 0 },
-                    { band: 2, gain: 0 },
-                    { band: 3, gain: 0 },
-                    { band: 4, gain: 0 },
-                    { band: 5, gain: 0 },
-                    { band: 6, gain: 0 },
-                    { band: 7, gain: 0 },
-                    { band: 8, gain: -0.25 },
-                    { band: 9, gain: -0.25 },    
-                    { band: 10, gain: -0.25 },    
-                    { band: 11, gain: -0.25 },
-                    { band: 12, gain: -0.25 },   
-                    { band: 13, gain: -0.25 },   
-                    { band: 14, gain: -0.25 } 
-                ];
-                player.setEQ(...bands);
-                let thing = new MessageEmbed()
-                    .setColor("#00FFD2")
-                    .setDescription(`${interaction.member} Applied the Filter Soft Mode`);
-                await interaction.reply({embeds: [thing]});
-            }
-            if(interaction.values[0] === 'seventh_option'){
-                player.clearEQ();
-                let thing = new MessageEmbed()
-                    .setColor("#00FFD2")
-                    .setDescription(`${interaction.member} Cleared all the filters`);
-                await interaction.reply({embeds: [thing]});
-            }
-        }
-    }
-        });
+   
     const row = new MessageActionRow()
     .addComponents(
         new MessageButton()
-            .setCustomId('Pausea')
+            .setCustomId('Pauseaa')
             .setLabel('Pause')
             .setStyle('SECONDARY')
             .setEmoji('876007266418057217'),
             new MessageButton()
-            .setCustomId('Resumea')
+            .setCustomId('Resumeaa')
             .setLabel('Resume')
             .setStyle('DANGER')
             .setEmoji('876019503325126667'),
             new MessageButton()
-            .setCustomId('Skipa')
+            .setCustomId('Skipaa')
             .setLabel('Skip')
             .setStyle('PRIMARY')
             .setEmoji('876007419724054548'),
             new MessageButton()
-            .setCustomId('Lyricsa')
+            .setCustomId('Lyricsaa')
             .setLabel('Lyrics')
             .setStyle('SUCCESS')
             .setEmoji('876007882578100264'),
             new MessageButton()
-            .setCustomId('Loopa')
+            .setCustomId('Loopaa')
             .setLabel('Loop')
             .setStyle('SECONDARY')
             .setEmoji('881060715941740555'),
@@ -352,7 +94,7 @@ tollector.on('collect', async interaction =>
     const column = new MessageActionRow()
     .addComponents(
             new MessageSelectMenu()
-            .setCustomId('filter')
+            .setCustomId('filtera')
             .setPlaceholder('Apply Filters')
             .addOptions([
                 {
