fc988d50ff381e82fe09746f26c65ee68b626bfc
Priyanshu360-cpu <tpriyanshu775@gmail.com>
2021-12-02 21:27:39 +0530

    selectSelfRoles => Multi thread initial commit

--- commands/Moderation/selectSelfRoles.js
+++ commands/Moderation/selectSelfRoles.js
@@ -16,38 +16,51 @@ module.exports = {
     execute: async (message, args, client, prefix) => {
         var a = await message.channel.send({content:"Self Roles#1"})
         let filter = m => message.author.id === m.author.id
-        let titleclr = message.channel.createMessageCollector({ filter, time: 30000, max: 1 })
         message.channel.send("Enter the number of Self Roles You want")
-
-            let b = []
-        for (let I = 0;I<client.data1;I++){
-            let filter = (m) => m.author.id === message.author.id
-            let titleclra = message.channel.createMessageCollector({ filter, time: 30000})
-            titleclra.on("collect", async (message) => {
-                message.channel.send(`Enter the name of next role and mention it`)
-                b.push[message.content]
-                titleclra.stop() 
-            })}
+        var i = 0;
+        var b = []; 
+        var c = 2;
+        var d;
+        let titleclr = message.channel.createMessageCollector({ filter, time: 30000, max: 1 })
+        titleclr.on("collect",async(message)=>{
+           c=message.content;
+            titleclr.stop()
+            message.channel.send(`Enter the name of next role and mention it`)
             
-            client.selfRoles = b;
-        titleclra.on('end', async (collected, reason) => {
-            console.log(b)
-            if (reason === 'time') {
-                
-              const content = new MessageButton()
-                .setLabel('Timeout')
-                .setStyle('DANGER')
-                .setCustomId('timeout|91817623842')
-                .setDisabled()
-  
-              const row = new MessageActionRow()
-                .addComponents([content])
-  const timeout = new MessageEmbed()
-  timeout.setTtitle("Time ran out, Try again");
-  timeout.setColor("RED")
-                a.edit({ embeds: [timeout], components: [row] })
-            }
-          })
+            let titleclra = message.channel.createMessageCollector({ filter, time: 30000, max: c})
+            titleclra.on("collect", async (message) => {
+                if(i!=2){
+                message.channel.send(`Enter the name of next role and mention it`)}
+                b[i] = message.content;
+                i=i+1;
+            }) 
+           for(j=1;j<=25;j++){
+               if(!client.selfRole.get(`${message.guild.id}_${j}`)){
+                   d = j;
+                   break;
+               }
+           } 
+           client.selfRole.set(`${message.guild.id}_${d}`,{d:{"Roles":b,"Id":b}})
+            titleclra.on('end', async (collected, reason) => {
+                if (reason === 'time') {
+                  const content = new MessageButton()
+                    .setLabel('Timeout')
+                    .setStyle('DANGER')
+                    .setCustomId('timeout|91817623842')
+                    .setDisabled()
+      
+                  const row = new MessageActionRow()
+                    .addComponents([content])
+      const timeout = new MessageEmbed()
+      timeout.setTtitle("Time ran out, Try again");
+      timeout.setColor("RED")
+                    a.edit({ embeds: [timeout], components: [row] })
+                }
+              })}
+              )
+            
+            
+        
             
         }
     }
